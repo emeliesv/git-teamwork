5ad840461b0506f5b9832e5aaa44c0c7227dd468
frodinger <frej.hedlin@gmail.com>
2024-05-21 13:44:39 +0200

    Header

---
+++ travelWithUs/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav>
+        <h1><Link to="/">Travel with Us</Link></h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/booking">Booking</Link>
+          </li>
+        </ul>
+        </nav>
+    )
+}
+
+export default Header;
