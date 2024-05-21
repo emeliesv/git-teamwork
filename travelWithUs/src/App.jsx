c7f1b30543853ae11933774af647c7a053e519c2
frodinger <frej.hedlin@gmail.com>
2024-05-21 13:56:16 +0200

    tog bort count

--- travelWithUs/src/App.jsx
+++ travelWithUs/src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
