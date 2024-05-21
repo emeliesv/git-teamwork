58a50101d0ea39e9f2cba415ed128bda1901a4d7
frodinger <frej.hedlin@gmail.com>
2024-05-21 13:52:14 +0200

    tagit bort app.css

--- travelWithUs/src/App.jsx
+++ travelWithUs/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 
